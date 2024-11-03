effe8361d87eb22b44b9c885133f8ff7788dc341
PotatoNotchips <95156385+PotatoNotchips@users.noreply.github.com>
2024-11-03 22:24:31 +0800

    Update rnd.py

--- rllte/xplore/reward/rnd.py
+++ rllte/xplore/reward/rnd.py
@@ -134,15 +134,11 @@ class RND(BaseReward):
             dist = F.mse_loss(src_feats, tgt_feats, reduction="none").mean(dim=1)
             print("This is  the dist b4 calculated from the mse loss: ", dist)
             print("This is  the dist b4 shape from the mse loss: ", dist.shape)
-            dist = th.mean(dist, dim=0)
+            dist = th.mean(dist, dim=1, keepdim=True)
             print("This is  the dist calculated from the mse loss: ", dist)
             print("This is  the dist shape from the mse loss: ", dist.shape)
             print("This is the n_steps needed for getting the int reward: ", n_steps)
             print("This is the n_envs needed for getting the int reward: ", n_envs)
-            print("This is  the dist calculated from the mse loss: ", dist)
-            print("This is  the dist shape from the mse loss: ", dist.shape)
-            print("This is the n_steps shape for getting the int reward: ", n_steps.shape)
-            print("This is the n_envs shape for getting the int reward: ", n_envs.shape)
             intrinsic_rewards = dist.view(n_steps, n_envs)
 
         # update the reward module
